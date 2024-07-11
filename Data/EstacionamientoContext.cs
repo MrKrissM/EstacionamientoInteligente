d1bcab3932fb795a2e71de674b9f5adab02cfc4e
Your Name <you@example.com>
2024-07-10 20:57:19 -0400

    agregado salida

--- Data/EstacionamientoContext.cs
+++ Data/EstacionamientoContext.cs
@@ -15,6 +15,7 @@ public class EstacionamientoContext : DbContext
     public DbSet<Entrada> Entradas { get; set; }
     public DbSet<Estancia> Estancias { get; set; }
     public DbSet<Salida> Salidas { get; set; }
+    public DbSet<ListadoCobro> ListadoCobros { get; set; }
 
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
